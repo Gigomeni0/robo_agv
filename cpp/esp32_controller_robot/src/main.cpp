0cb07df2fd5aa0f62949ec217261d7ae315627ac
Gigomeni0 <joaoantonio.goncalves2003@gmail.com>
2025-03-04 19:04:51 -0300

    Configuração de MQTT e assinatura de tópico robo_gaveteiro/comandos

--- cpp/esp32_controller_robot/src/main.cpp
+++ cpp/esp32_controller_robot/src/main.cpp
@@ -3,96 +3,142 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 
-// Pinos do enco
+// Cliente Wi-Fi
+WiFiClient espClient;
+// Cliente MQTT
+PubSubClient client(espClient);
+
+const char* topico = "robo_gaveteiro/comandos";
+
+// Pinos do encoder
 #define ENCODER_A 4
 #define ENCODER_B 5
 #define ENCODER_A2 6
 #define ENCODER_B2 7
 
-// Pinos da ponte H
+// Pinos da Ponte H
 #define IN1 15
 #define IN2 16
 #define IN3 17
 #define IN4 18
 
-
-// Variáveis para o encoder
+// Variáveis do encoder
 volatile long pulseCount = 0;
-int pulsesPerRevolution = 11; // 11 pulsos por revolução
+int pulsesPerRevolution = 11;
 float rpm = 0;
 unsigned long lastTime = 0;
 
 // Parâmetros da roda
 float wheelDiameter = 12;                                          // Diâmetro da roda em cm
-float wheelCircumference = PI * wheelDiameter;                     // Circunferência em cm
-float distancePerPulse = wheelCircumference / pulsesPerRevolution; // Distância por pulso em cm
-float totalDistance = 0;                                           // Distância total percorrida em cm
+float wheelCircumference = PI * wheelDiameter;                     // Circunferência
+float distancePerPulse = wheelCircumference / pulsesPerRevolution; // Distância por pulso
+float totalDistance = 0;
 
-//============================== Funções do programa ==========================================
-// Função de interrupção para o encoder
-void encoderISR()
-{
+// Função de interrupção do encoder
+void encoderISR() {
   pulseCount++;
 }
 
-// Função para conectar-se a uma rede Wi-Fi
-void connect(String SSID, String password)
-{
-  WiFi.begin(SSID.c_str(), password.c_str());
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.println("Conectando à rede Wi-Fi...");
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensagem recebida no tópico: ");
+  Serial.println(topic);
+
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
   }
-  Serial.println("Conectado à rede Wi-Fi!");
+
+  Serial.print("Mensagem: ");
+  Serial.println(message);
+
+  if (message == "ligar") {
+    Serial.println("Ligando motores...");
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } 
+  else if (message == "desligar") {
+    Serial.println("Desligando motores...");
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
 }
 
-void setup()
-{
-  connect("Gigo2.4G", "18253122Ro");
-  // Configura os pinos do encoder como entrada
+void connectToWifi(const char* SSID, const char* password) {
+  Serial.println("Conectando ao Wi-Fi...");
+  WiFi.begin(SSID, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("\nWi-Fi conectado!");
+}
+
+void connectToMQTT(const char* clientID, const char* mqttServer, int mqttPort, const char* user, const char* password) {
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+
+  while (!client.connected()) {
+    Serial.print("Conectando ao Broker MQTT...");
+    if (client.connect(clientID, user, password)) {
+      Serial.println(" Conectado!");
+      client.subscribe(topico);
+      Serial.print("Inscrito no tópico: ");
+      Serial.println(topico);
+    } 
+    else {
+      Serial.print(" Falha, rc=");
+      Serial.print(client.state());
+      Serial.println(" Tentando novamente em 5 segundos...");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectToWifi("Gigo2.4G", "18253122Ro");
+  connectToMQTT("ESP32Client", "test.mosquitto.org", 1883, "espClient", "");
+
+  // Configuração dos pinos
   pinMode(ENCODER_A, INPUT);
   pinMode(ENCODER_B, INPUT);
-
-  // Configura os pinos da ponte H como saída
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
-  // Configura a interrupção para o encoder
   attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoderISR, RISING);
 
-  // Inicia a comunicação serial
-  Serial.begin(9600);
-
-  digitalWrite(IN1, HIGH);
+  digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
-void loop()
-{
-  // Calcula a RPM
+void loop() {
+  if (!client.connected()) {
+    connectToMQTT("ESP32Client", "test.mosquitto.org", 1883, "espClient", "");
+  }
+
+  client.loop();  // Mantém a conexão ativa e escuta mensagens
+
   unsigned long currentTime = millis();
   unsigned long timeElapsed = currentTime - lastTime;
 
-  if (timeElapsed >= 1000)
-  { // Atualiza a cada segundo
+  if (timeElapsed >= 1000) {
     rpm = (float)(pulseCount * 60) / (float)pulsesPerRevolution;
-    totalDistance += pulseCount * distancePerPulse; // Atualiza a distância total
+    totalDistance += pulseCount * distancePerPulse;
     pulseCount = 0;
     lastTime = currentTime;
 
-    // Exibe a RPM e a distância no monitor serial
     Serial.print("RPM: ");
     Serial.print(rpm);
     Serial.print(" | Distância total: ");
     Serial.print(totalDistance);
     Serial.println(" cm");
   }
-
-  // Exemplo de controle do motor
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
 }
-
